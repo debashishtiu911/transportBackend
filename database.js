ecb3199ffd2dee567654a664021a5932d327d859
debashishtiu911 <debashishtiu14@gmail.com>
2024-06-24 23:02:20 +0530

    hello Motto

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 //const mongoURL='mongodb://127.0.0.1:27017/transport'
 const mongoURL =process.env.MONGO_URL
-
+//helllo 
 mongoose.connect(mongoURL, {
 });
 
